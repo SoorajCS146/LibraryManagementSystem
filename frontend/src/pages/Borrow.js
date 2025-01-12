e1554fd115126d693e5d1bc83adac8ec824f92b3
Manvith Rai <manvithrai16@gmail.com>
2025-01-12 19:23:33 +0530

    Adding the Pages section of the Project

---
+++ frontend/src/pages/Borrow.js
@@ -0,0 +1,12 @@
+import React from "react";
+import BorrowBook from "../components/BorrowBook";
+
+function Borrow() {
+  return (
+    <div className="page-container">
+      <BorrowBook />
+    </div>
+  );
+}
+
+export default Borrow;
---
+++ frontend/src/pages/Notifications.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Notification from "../components/Notification";
+
+function Notifications() {
+  return (
+    <div className="page-container">
+      <Notification />
+    </div>
+  );
+}
+
+export default Notifications;
